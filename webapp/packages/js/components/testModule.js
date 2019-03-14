f47f651d79e8da1b782735626093811b1e241281
Mario <mario.simaska@gmail.com>
2019-03-14 23:02:33 +0000

    finished add to cart functionality

--- webapp/packages/js/components/testModule.js
+++ webapp/packages/js/components/testModule.js
@@ -4,17 +4,41 @@ export default function testModule () {
     const miniCart = document.querySelector('.header__minicart');
     const addToCart = document.querySelector('.button--add-to-cart');
     
-    // Convert to String
     const ConvertToString = (value) => {
         return Number(value.replace(/[^0-9.-]+/g,""));
     }
 
+
+
+    const createBasket = () => {
+        const shoppingBasket = document.querySelector('.cartMessage');
+        const cartItems = document.querySelector('.shopping__cart-items');
+        const cartSum = document.querySelector('.stotal');
+
+        if (sessionStorage.basket) {
+            const BasketData = JSON.parse( sessionStorage.getItem('basket'));
+            const BasketTotal = JSON.parse( sessionStorage.getItem('CartTotal'));
+            cartItems.innerHTML = "";
+            
+            BasketData.forEach(function(element, index) {
+                const cartHtml = "<tr><td>"+ element.name +"</td><td>"+ element.price +"</td><td>"+ element.quantity +"</td><td>"+ element.subtotal +"</td></tr>";
+                cartItems.insertAdjacentHTML('afterbegin',cartHtml);
+            });
+
+            cartSum.innerHTML = BasketTotal.toFixed(2);
+
+        }
+        else {
+            shoppingBasket.innerHTML = "You have no items in your cart";
+        }
+
+    }
+
     // https://www.w3resource.com/javascript-exercises/javascript-array-exercise-10.php
     // http://jsfiddle.net/greenrhino/rS3Qf/
-    // Set up basket items array
-    const createArray = () => {
+    
+    const addToBasket = () => {
 
-        // 2 version
         const myItem = {};
         myItem.name = document.querySelector('.product__title').innerHTML;
         myItem.price = ConvertToString(document.querySelector('.product__price--current').innerHTML);
@@ -32,7 +56,6 @@ export default function testModule () {
 
         let totalPrice = basket.map(obj => obj.subtotal).reduce((acc, next) => acc + next);
         sessionStorage.setItem( "CartTotal", JSON.stringify(totalPrice) );
-
     }
 
     // https://www.telerik.com/blogs/functional-programming-with-javascript-object-arrays
@@ -42,19 +65,9 @@ export default function testModule () {
 
     addToCart.addEventListener('click', (e)=> {
         e.preventDefault();
-
-        // const myItem = {}
-        // myItem.name = document.querySelector('.product__title').innerHTML;
-        // myItem.price = ConvertToString(document.querySelector('.product__price--current').innerHTML);
-        // myItem.quantity = document.querySelector('.product__quantity-holder input').value;
-        // myItem.subtotal = myItem.price * myItem.quantity;
-
-        // myArray.push(myItem);
-        // sessionStorage.setItem( "CartItems", JSON.stringify(myArray) );
-
-        // let totalPrice = myArray.map(obj => obj.subtotal).reduce((acc, next) => acc + next);
-        // sessionStorage.setItem( "CartTotal", JSON.stringify(totalPrice) );
-
-        createArray();
+        addToBasket();
+        createBasket();
     })
+
+    createBasket();
 };
